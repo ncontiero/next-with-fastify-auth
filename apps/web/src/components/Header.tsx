fa58aa62b6de8b829f50b3a96e3c1be82d0584d2
dkshs <88355080+dkshs@users.noreply.github.com>
2024-06-13 17:59:31 -0300

    chore(web): use custom link component in header

--- apps/web/src/components/Header.tsx
+++ apps/web/src/components/Header.tsx
@@ -1,8 +1,8 @@
 import type { User } from "@/utils/types";
 import Image from "next/image";
-import Link from "next/link";
 import { fetcher } from "@/utils/fetcher";
 import { ProfileButton } from "./ProfileButton";
+import { Link } from "./ui/Link";
 
 export async function Header() {
   const { data: user } = await fetcher<User>("auth/profile", {
@@ -13,10 +13,7 @@ export async function Header() {
     <header className="fixed inset-x-0 z-[999] h-[72px] border-b bg-secondary/60 backdrop-blur-md">
       <nav className="m-auto flex size-full max-w-[1600px] items-center justify-between gap-2 px-6">
         <div className="flex items-center gap-3">
-          <Link
-            href="/"
-            className="rounded-full ring-ring duration-200 hover:brightness-150 focus:outline-none focus:ring-2"
-          >
+          <Link href="/" className="rounded-full hover:brightness-150">
             <Image
               src="/icon"
               alt="icon"
@@ -33,13 +30,13 @@ export async function Header() {
           <div className="flex items-center gap-3">
             <Link
               href="/auth/sign-in"
-              className="rounded-md p-2 font-bold ring-ring duration-200 hover:text-foreground/60 focus:outline-none focus:ring-2"
+              className="rounded-md p-2 font-bold text-foreground hover:text-foreground/60"
             >
               Sign In
             </Link>
             <Link
               href="/auth/sign-up"
-              className="rounded-md p-2 font-bold ring-ring duration-200 hover:text-foreground/60 focus:outline-none focus:ring-2"
+              className="rounded-md p-2 font-bold text-foreground hover:text-foreground/60"
             >
               Sign Up
             </Link>
