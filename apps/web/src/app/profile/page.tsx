417dc71138c7517856b00041949988d34ba8b2c1
dkshs <88355080+dkshs@users.noreply.github.com>
2024-06-14 19:23:29 -0300

    fix(web): update tailwindcss class order

--- apps/web/src/app/profile/page.tsx
+++ apps/web/src/app/profile/page.tsx
@@ -22,20 +22,20 @@ export default async function ProfilePage() {
   }
 
   return (
-    <div className="max-w-2xl mx-auto mt-16 flex flex-col justify-center px-4">
-      <div className="flex flex-col p-6 border rounded-md">
+    <div className="mx-auto mt-16 flex max-w-2xl flex-col justify-center px-4">
+      <div className="flex flex-col rounded-md border p-6">
         <h1 className="text-4xl font-bold">Profile</h1>
         {user.name ? (
-          <p className="mt-4 text-xl flex items-center gap-2">
+          <p className="mt-4 flex items-center gap-2 text-xl">
             <Label htmlFor="name">Name: </Label>
             <Input id="name" type="text" value={user.name} />
           </p>
         ) : null}
-        <p className="mt-4 text-xl flex items-center gap-2">
+        <p className="mt-4 flex items-center gap-2 text-xl">
           <Label htmlFor="email">Email: </Label>
           <Input id="email" type="email" value={user.email} />
         </p>
-        <Button asChild className="flex mt-4 w-fit">
+        <Button asChild className="mt-4 flex w-fit">
           <Link
             href="/account/update-password"
             className="text-foreground hover:no-underline"
